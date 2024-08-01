7d0e63219fcc1590f536162cd74863c90b67ba88
Théo <tn-pro@pm.me>
2024-08-01 02:51:14 +0200

    push

--- helpers/qdrant_helper.py
+++ helpers/qdrant_helper.py
@@ -5,7 +5,7 @@ from qdrant_client import QdrantClient
 
 def setup_client_qdrant():
     client_qdrant = QdrantClient(
-        url="https://ddffeb25-284c-447c-b81c-541719112ac9.us-east4-0.gcp.cloud.qdrant.io",
+        url=os.getenv("QDRANT_HOST"),
         port=6333,
         api_key=os.getenv("QDRANT_API_KEY"), )
     return client_qdrant
